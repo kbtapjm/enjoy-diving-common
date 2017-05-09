f1efc9eaef05bb8004649911176c2a8420e9400b
jmpark <kbtapjm@gmail.com>
2017-05-10 00:30:31 +0900

    #3 유저관리 테스트 케이스 작성 및 Querydsl 설정 추가

--- src/main/java/kr/co/pjm/diving/common/domain/enumeration/GenderEnum.java
+++ src/main/java/kr/co/pjm/diving/common/domain/enumeration/GenderEnum.java
@@ -2,24 +2,24 @@ package kr.co.pjm.diving.common.domain.enumeration;
 
 /**
  * <pre>
- * @Package Name : kr.co.pjm.diving.web.domain.entity
- * @Class Name : UserStatusEnum.java
+ * @Package Name : kr.co.pjm.diving.common.domain.enumeration
+ * @Class Name : GenderEnum.java
  * </pre>
  * 
  * @author : jmpark
- * @Date : 2017. 5. 5.
+ * @Date : 2017. 5. 9.
  * @Version : 1.0
- * @Description : 유저 상태(NORMAL : 정상 회원, WITHDRAWAL : 탈퇴 회원) 
+ * @Description : 성별 
  *
  */
-public enum UserStatusEnum {
-  
-  NORMAL(0, "NORMAL"), WITHDRAWAL(1, "WITHDRAWAL");
+public enum GenderEnum {
+
+  MALE(0, "MALE"), FEMALE(1, "FEMALE");
   
   private int code;
   private String description;
   
-  private UserStatusEnum(int code, String description) {
+  private GenderEnum(int code, String description) {
     this.code = code;
     this.description = description;
   }
@@ -31,5 +31,4 @@ public enum UserStatusEnum {
   public String getDescription() {
     return description;
   }
-
 }
