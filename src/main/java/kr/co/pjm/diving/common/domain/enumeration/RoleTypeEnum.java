67a70e9b055c644a5480c84a09a7072e31667357
jmpark <kbtapjm@gmail.com>
2017-10-15 10:42:48 +0900

    롤 타입 변경

--- src/main/java/kr/co/pjm/diving/common/domain/enumeration/RoleTypeEnum.java
+++ src/main/java/kr/co/pjm/diving/common/domain/enumeration/RoleTypeEnum.java
@@ -3,75 +3,74 @@ package kr.co.pjm.diving.common.domain.enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /**
  * <pre>
- * @Package Name : kr.co.pjm.diving.common.domain.enumeration
- * @Class Name : RoleTypeEnum.java
+ * &#64;Package Name : kr.co.pjm.diving.common.domain.enumeration
+ * &#64;Class Name : RoleTypeEnum.java
  * </pre>
  * 
  * @author : jmpark
  * @Date : 2017. 5. 4.
  * @Version : 1.0
- * @Description : 유저 ROLE 
+ * @Description : 유저 ROLE
  *
  */
 public enum RoleTypeEnum {
-  
+
   ADMIN(100, "ADMIN"), MANAGER(200, "MANAGER"), USER(300, "USER");
-  
+
   private int code;
   private String description;
 
   private RoleTypeEnum(int code, String description) {
-      this.code = code;
-      this.description = description;
+    this.code = code;
+    this.description = description;
   }
 
   public int getCode() {
-      return code;
+    return code;
   }
 
   public String getDescription() {
-      return description;
+    return description;
   }
 
   public String getName() {
-      return name();
+    return name();
   }
 
   private static final Map<Integer, RoleTypeEnum> valueAndTypeMap = new HashMap<>();
   private static final Map<String, RoleTypeEnum> descriptionAndTypeMap = new HashMap<>();
 
   static {
-      for (RoleTypeEnum type : values()) {
-          valueAndTypeMap.put(type.getCode(), type);
-      }
+    for (RoleTypeEnum type : values()) {
+      valueAndTypeMap.put(type.getCode(), type);
+    }
 
-      for (RoleTypeEnum type : values()) {
-          descriptionAndTypeMap.put(type.getDescription(), type);
-      }
+    for (RoleTypeEnum type : values()) {
+      descriptionAndTypeMap.put(type.getDescription(), type);
+    }
   }
 
   public static RoleTypeEnum findByValue(int value) {
-      return valueAndTypeMap.get(value);
+    return valueAndTypeMap.get(value);
   }
 
   public static RoleTypeEnum findByDescription(String description) {
-      return descriptionAndTypeMap.get(description);
+    return descriptionAndTypeMap.get(description);
   }
 
   public static RoleTypeEnum valueOf(int value) {
-      switch (value) {
+    switch (value) {
       case 100:
-          return RoleTypeEnum.ADMIN;
+        return RoleTypeEnum.ADMIN;
       case 200:
-          return RoleTypeEnum.MANAGER;
+        return RoleTypeEnum.MANAGER;
       case 999:
-          return RoleTypeEnum.USER;
+        return RoleTypeEnum.USER;
       default:
-          throw new AssertionError("Unknown RoleType : " + value);
-      }
+        throw new AssertionError("Unknown RoleType : " + value);
+    }
   }
 
 }
