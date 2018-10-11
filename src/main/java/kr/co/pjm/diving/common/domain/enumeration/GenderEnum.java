162d75a3d878ac9e2c2e0e25d002b92861494646
kbtapjm <kbtapjm@gmail.com>
2018-10-11 14:06:27 +0900

    update users

--- src/main/java/kr/co/pjm/diving/common/domain/enumeration/GenderEnum.java
+++ src/main/java/kr/co/pjm/diving/common/domain/enumeration/GenderEnum.java
@@ -1,20 +1,10 @@
 package kr.co.pjm.diving.common.domain.enumeration;
 
-/**
- * <pre>
- * @Package Name : kr.co.pjm.diving.common.domain.enumeration
- * @Class Name : GenderEnum.java
- * </pre>
- * 
- * @author : jmpark
- * @Date : 2017. 5. 9.
- * @Version : 1.0
- * @Description : 성별 
- *
- */
-public enum GenderEnum {
+import java.util.HashMap;
+import java.util.Map;
 
-  MALE(0, "MALE"), FEMALE(1, "FEMALE");
+public enum GenderEnum {
+  MALE(0, "남성"), FEMALE(1, "여성");
   
   private int code;
   private String description;
@@ -30,5 +20,17 @@ public enum GenderEnum {
 
   public String getDescription() {
     return description;
+  }
+  
+  private static final Map<Integer, GenderEnum> valueAndGenderMap = new HashMap<>();
+  
+  static {
+    for (GenderEnum genderEnum : values()) {
+      valueAndGenderMap.put(genderEnum.code, genderEnum);
+    }
+  }
+  
+  public static GenderEnum findByValue(Integer value) {
+    return valueAndGenderMap.get(value);
   }
 }
