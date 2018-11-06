901090babde32d2036c2004ce6f786f0f40a37f8
jmpark <kbtapjm@gmail.com>
2018-11-07 07:57:55 +0900

    빌더패턴으로 변경

--- src/main/java/kr/co/pjm/diving/common/domain/dto/ErrorDto.java
+++ src/main/java/kr/co/pjm/diving/common/domain/dto/ErrorDto.java
@@ -1,23 +1,28 @@
 package kr.co.pjm.diving.common.domain.dto;
 
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@JsonInclude(Include.NON_NULL)
 @NoArgsConstructor
-@AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class ErrorDto {
   private String message;
   private int status;
   private String path;
-  private Date timestamp;
+  //private Date timestamp;
+
+  @Builder
+  public ErrorDto(String message, int status, String path) {
+    this.message = message;
+    this.status = status;
+    this.path = path;
+  }
+  
 }
