fd4a92f57ff128913df7f7c2bd156fd92afce83f
kbtapjm <kbtapjm@gmail.com>
2019-01-18 14:27:48 +0900

    클라이언트 ip 주소 가져오는 함수 추가

--- src/main/java/kr/co/pjm/diving/common/util/CommonUtil.java
+++ src/main/java/kr/co/pjm/diving/common/util/CommonUtil.java
@@ -3,6 +3,12 @@ package kr.co.pjm.diving.common.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 public class CommonUtil {
   private CommonUtil() {
   }
@@ -19,20 +25,38 @@ public class CommonUtil {
     String result = null;
 
     try {
-      result = URLEncoder.encode(s, "UTF-8")
-          .replaceAll("\\+", "%20")
-          .replaceAll("\\%21", "!")
-          .replaceAll("\\%27", "'")
-          .replaceAll("\\%28", "(")
-          .replaceAll("\\%29", ")")
-          .replaceAll("\\%7E", "~");
+      result = URLEncoder.encode(s, "UTF-8").replaceAll("\\+", "%20").replaceAll("\\%21", "!").replaceAll("\\%27", "'")
+          .replaceAll("\\%28", "(").replaceAll("\\%29", ")").replaceAll("\\%7E", "~");
     } catch (UnsupportedEncodingException e) {
       e.printStackTrace();
-      
+
       result = s;
     }
 
     return result;
+  }
+
+  public String getRemoteIp() {
+    HttpServletRequest request = ((ServletRequestAttributes )RequestContextHolder.currentRequestAttributes()).getRequest();
+    
+    String ip = request.getHeader("X-Forwarded-For");
+    if (ip == null) {
+      ip = request.getHeader("Proxy-Client-IP");
+    }
+    if (ip == null) {
+      ip = request.getHeader("WL-Proxy-Client-IP"); // weblogic
+    }
+    if (ip == null) {
+      ip = request.getHeader("HTTP_CLIENT_IP");
+    }
+    if (ip == null) {
+      ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+    }
+    if (ip == null) {
+      ip = request.getRemoteAddr();
+    }
+
+    return StringUtils.EMPTY;
   }
 
 }
