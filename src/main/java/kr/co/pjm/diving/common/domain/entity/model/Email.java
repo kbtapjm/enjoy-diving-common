cd276a45ca1e5743ebcc9e609db521169c222906
kbtapjm <kbtapjm@gmail.com>
2019-03-22 16:15:42 +0900

    유틸 수정

---
+++ src/main/java/kr/co/pjm/diving/common/domain/entity/model/Email.java
@@ -0,0 +1,38 @@
+package kr.co.pjm.diving.common.domain.entity.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonIgnoreProperties({"host", "id"})
+public class Email {
+    
+    @org.hibernate.validator.constraints.Email
+    @Column(name = "email", nullable = false, unique = true)
+    private String value;
+    
+    @Builder
+    public Email(String value) {
+        this.value = value;
+    }
+
+    public String getHost() {
+        int index = value.indexOf("@");
+        return value.substring(index);
+    }
+
+    public String getId() {
+        int index = value.indexOf("@");
+        return value.substring(0, index);
+    }
+
+}
--- src/main/java/kr/co/pjm/diving/common/util/DateUtil.java
+++ src/main/java/kr/co/pjm/diving/common/util/DateUtil.java
@@ -1,18 +1,24 @@
 package kr.co.pjm.diving.common.util;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class DateUtil {
   public static final String FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
-  
+  public static final String FORMAT_YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
   private DateUtil() {}
-  
+
   private static class DateUtilHolder {
     static final DateUtil single = new DateUtil();
   }
-  
+
   public static DateUtil getInstance() {
     return DateUtilHolder.single;
   }
@@ -21,12 +27,36 @@ public class DateUtil {
     Date dt = null;
     try {
       SimpleDateFormat sdf = new SimpleDateFormat(format);
-      
+
       dt = sdf.parse(dateStr);
     } catch (ParseException e) {
       e.printStackTrace();
     }
-    
+
     return dt;
+  }
+
+  public String getTimestampToDateString(long time, String format) {
+    SimpleDateFormat formatter = new SimpleDateFormat(format);
+
+    Timestamp currentTime = new Timestamp(time);
+    String dateStr = formatter.format(currentTime);
+
+    return dateStr;
+
+  }
+
+  public String getDateToString(Date date, String format) {
+    if (date == null)
+      return StringUtils.EMPTY;
+
+    SimpleDateFormat formatter = new SimpleDateFormat(format);
+    String dateStr = formatter.format(date);
+
+    return dateStr;
+  }
+
+  public String getLocalDateToString(long daysToSubtract, String format) {
+    return LocalDate.now().minusDays(daysToSubtract).format(DateTimeFormatter.ofPattern(format));
   }
 }
